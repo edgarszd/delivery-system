8f0ca81917c617b5bf694abeb2dc552c17502fc1
Edgar <edgarsz@academico.ufs.br>
2025-12-22 01:00:47 -0300

    refactor: remove console error

--- src/app.ts
+++ src/app.ts
@@ -7,36 +7,33 @@ import { RestaurantControllerFactory } from './configurations/factory/restaurant
 import { OpenApiHttpError } from './application/exceptions/http.error';
 
 export function createApp(): Express {
-    const SPEC_PATH = './src/contracts/contract.yaml';
-    
-    const file = fs.readFileSync(SPEC_PATH, 'utf8');
-    const swaggerDocument = YAML.parse(file);
-    
-    const app = express();
+  const SPEC_PATH = './src/contracts/contract.yaml';
 
-    app.use(express.json());
-    
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    
-    app.use(
-      OpenApiValidator.middleware({
-        apiSpec: SPEC_PATH,
-        validateResponses: true,
-      })
-    );
-    
-    app.use('/', RestaurantControllerFactory.create().getRoutes());
-    
-    app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-      console.error(err);
-      if(err instanceof OpenApiHttpError) {
-        return res.status(err.status).json({ message: err.message });
-      }
-    
-      return res
-        .status(500)
-        .json({ message: 'An unexpected error occurred!' });
-    });
+  const file = fs.readFileSync(SPEC_PATH, 'utf8');
+  const swaggerDocument = YAML.parse(file);
 
-    return app;
+  const app = express();
+
+  app.use(express.json());
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  app.use(
+    OpenApiValidator.middleware({
+      apiSpec: SPEC_PATH,
+      validateResponses: true,
+    }),
+  );
+
+  app.use('/', RestaurantControllerFactory.create().getRoutes());
+
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof OpenApiHttpError) {
+      return res.status(err.status).json({ message: err.message });
+    }
+
+    return res.status(500).json({ message: 'An unexpected error occurred!' });
+  });
+
+  return app;
 }
