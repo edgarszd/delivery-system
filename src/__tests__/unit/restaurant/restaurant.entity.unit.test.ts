86a8d82829c91d9bb8ae8814cdc00582d6ff0d2e
Edgar <edgarsz@academico.ufs.br>
2025-12-22 00:12:00 -0300

    feat: adds the restaurant entity test

---
+++ src/__tests__/unit/restaurant/restaurant.entity.unit.test.ts
@@ -0,0 +1,64 @@
+import { IRestaurant } from '../../../domain/restaurant/entity/interfaces/restaurant.interface';
+import { RestaurantEntity } from '../../../domain/restaurant/entity/restaurant.entity';
+
+let restaurant: IRestaurant;
+
+beforeEach(() => {
+  restaurant = {
+    _id: 'valid id',
+    name: 'Restaurante X',
+    address: 'Rua A, 133',
+  };
+});
+
+describe('Testing RestaurantEntity', () => {
+  describe('Success cases', () => {
+    describe('When the object is valid', () => {
+      it('should return a valid instance', () => {
+        const restaurantInstance = new RestaurantEntity(restaurant);
+
+        expect(restaurantInstance._id).toBe(restaurant._id);
+        expect(restaurantInstance.name).toBe(restaurant.name);
+        expect(restaurantInstance.address).toBe(restaurant.address);
+      });
+
+      it('should return a valid instance without _id', () => {
+        delete restaurant._id;
+
+        const restaurantInstance = new RestaurantEntity(restaurant);
+
+        expect(restaurantInstance._id).toBeUndefined();
+        expect(restaurantInstance.name).toBe(restaurant.name);
+        expect(restaurantInstance.address).toBe(restaurant.address);
+      });
+    });
+  });
+
+  describe('Error cases', () => {
+    describe('When the name is empty', () => {
+      it.each(['', ' '])(
+        'should throw an error for empty name',
+        (emptyName: string) => {
+          restaurant.name = emptyName;
+
+          expect(() => new RestaurantEntity(restaurant)).toThrow(
+            /^The restaurant's name cannot be empty!$/,
+          );
+        },
+      );
+    });
+
+    describe('When the address is empty', () => {
+      it.each(['', ' '])(
+        'should throw an error for empty address',
+        (emptyAddress: string) => {
+          restaurant.address = emptyAddress;
+
+          expect(() => new RestaurantEntity(restaurant)).toThrow(
+            /^The restaurant's address cannot be empty!$/,
+          );
+        },
+      );
+    });
+  });
+});
