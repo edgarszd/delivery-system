59e6a5b04446dbb482167bf9e3197ef15a2e46b3
Edgar <edgarsz@academico.ufs.br>
2025-12-22 01:05:43 -0300

    test: implements integration tests for restaurant creation

---
+++ src/__tests__/integration/restaurant/controller/restaurant.create.int.test.ts
@@ -0,0 +1,80 @@
+import request from 'supertest';
+import { app } from '../../../../../jest/setup-integration-tests';
+import { IRestaurant } from '../../../../domain/restaurant/entity/interfaces/restaurant.interface';
+import { MRestaurant } from '../../../../infrastructure/database/mongo/schemas/restaurant.schema';
+
+let restaurant: IRestaurant;
+
+beforeEach(() => {
+  restaurant = {
+    name: 'Restaurant X',
+    address: 'Rua A, 133',
+  };
+});
+
+describe('Create Restaurant - Integration Tests', () => {
+  it(`Should create restaurant successfully with valid data
+    status code: 201
+    route: POST /restaurants`, async () => {
+    const response = await request(app).post('/restaurants').send(restaurant);
+
+    const createdRestaurant = await MRestaurant.findById(
+      response.body._id,
+    ).lean();
+
+    expect(response.body).toMatchObject({
+      name: restaurant.name,
+      address: restaurant.address,
+    });
+    expect(response.status).toBe(201);
+
+    expect(createdRestaurant).toBeTruthy();
+  });
+
+  it(`Should return error when name is empty
+  status code: 422
+  route: POST /restaurants`, async () => {
+    restaurant.name = '';
+
+    const response = await request(app).post('/restaurants').send(restaurant);
+
+    expect(response.status).toBe(422);
+  });
+
+  it(`Should return error when address is empty
+  status code: 422
+  route: POST /restaurants`, async () => {
+    restaurant.address = '';
+
+    const response = await request(app).post('/restaurants').send(restaurant);
+
+    expect(response.status).toBe(422);
+  });
+
+  it(`Should return error when required fields are missing
+  status code: 400
+  route: POST /restaurants`, async () => {
+    const incompleteRestaurant = { name: 'Restaurant' };
+
+    const response = await request(app)
+      .post('/restaurants')
+      .send(incompleteRestaurant);
+
+    expect(response.status).toBe(400);
+  });
+
+  it(`Should return error when additional fields exist
+  status code: 400
+  route: POST /restaurants`, async () => {
+    const restaurantWithExtraFields = {
+      ...restaurant,
+      extraField: 'extra',
+    };
+
+    const response = await request(app)
+      .post('/restaurants')
+      .send(restaurantWithExtraFields);
+
+    expect(response.status).toBe(400);
+  });
+});
