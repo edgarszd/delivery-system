f8cf43ba9cd78ad2a45a94927406bd098ad153ad
Edgar <edgarsz@academico.ufs.br>
2025-12-22 00:26:45 -0300

    chore: create app and server bootstrap structure

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!")
+import mongoose from 'mongoose';
+import { createApp } from './app';
+import './configurations/dotenv';
+import * as env from './configurations/env-constants';
+
+const app = createApp();
+
+mongoose.connect(env.DATABASE_URI, { dbName: env.DATABASE_NAME });
+
+app.listen(env.PORT, () => {
+  console.log(`Delivery System listening on port ${env.PORT}`);
+});
