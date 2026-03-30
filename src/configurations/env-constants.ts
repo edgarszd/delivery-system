d2edc1c06fbfba563843fb2150d7b29d670685f9
Edgar <edgarsz@academico.ufs.br>
2026-03-29 22:00:05 -0300

    fix: prevent undefined from being cast to string

--- src/configurations/env-constants.ts
+++ src/configurations/env-constants.ts
@@ -1,4 +1,4 @@
 export const PORT = Number(process.env.PORT) || 3000;
 
-export const DATABASE_URI = String(process.env.DATABASE_URI);
-export const DATABASE_NAME = String(process.env.DATABASE_NAME);
+export const DATABASE_URI = process.env.DATABASE_URI;
+export const DATABASE_NAME = process.env.DATABASE_NAME;
