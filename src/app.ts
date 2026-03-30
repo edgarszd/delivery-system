c8ba2bf7bda31f856aaec45c43f1aaf31ce7f311
Edgar <edgarsz@academico.ufs.br>
2026-03-29 21:58:33 -0300

    refactor: encapsulate application setup in App class

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,110 @@
-import fs from 'fs';
-import express, { Express, Request, Response, NextFunction } from 'express';
+import { readFileSync } from 'fs';
+import express, { Request, Response, NextFunction, RequestHandler, Application } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import * as YAML from 'yaml';
 import * as OpenApiValidator from 'express-openapi-validator';
-import { RestaurantControllerFactory } from './configurations/factory/restaurant/restaurant.controller.factory';
 import { OpenApiHttpError } from './application/exceptions/http.error';
-import { CategoryControllerFactory } from './configurations/factory/category/category.controller.factory';
-import { ProductControllerFactory } from './configurations/factory/product/product.controller.factory';
-import { OrderControllerFactory } from './configurations/factory/order/order.controller.factory';
+import mongoose from 'mongoose';
+import { IController } from './application/controllers/controller.interface';
 
-export function createApp(): Express {
-  const SPEC_PATH = './src/contracts/contract.yaml';
+export class App {
+  app: Application;
 
-  const file = fs.readFileSync(SPEC_PATH, 'utf8');
-  const swaggerDocument = YAML.parse(file);
+  port: number;
 
-  const app = express();
+  apiSpecLocation?: string;
 
-  app.use(express.json());
+  database?: { 
+    dbURI?: string;
+    dbName?: string;
+  }
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  middlewares: Array<RequestHandler> = [
+    express.json({ limit: '3mb' }),
+  ];
+  
+  constructor(appConfig: {
+    port: number;
+    apiSpecLocation?: string;
+    middlewares?: Array<RequestHandler>;
+    controllers?: Array<IController>;
+    database?: { 
+      dbURI?: string;
+      dbName?: string;
+    };
+  }) {
+    this.app = express();
+    this.port = appConfig.port;
+    this.apiSpecLocation = appConfig.apiSpecLocation;
+    this.database = appConfig.database;
+    this.middlewares = this.middlewares.concat(appConfig.middlewares || []);
 
-  app.use(
-    OpenApiValidator.middleware({
-      apiSpec: SPEC_PATH,
-      validateResponses: true,
-    }),
-  );
+    this.useMiddlewares(this.middlewares);
+    
+    this.useRoutes(appConfig.controllers || []);
 
-  app.use('/', RestaurantControllerFactory.create().getRoutes());
-  app.use('/', CategoryControllerFactory.create().getRoutes());
-  app.use('/', ProductControllerFactory.create().getRoutes());
-  app.use('/', OrderControllerFactory.create().getRoutes());
+    this.useCustomizers();
+  }
 
-  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof OpenApiHttpError) {
-      return res.status(err.status).json({ message: err.message });
+  private useMiddlewares(middlewares: Array<RequestHandler>) {
+    middlewares.forEach((middleware) => this.app.use(middleware));
+
+    if (!this.apiSpecLocation) {
+      return;
     }
 
-    return res.status(500).json({ message: 'An unexpected error occurred!' });
-  });
+    this.app.use(
+      OpenApiValidator.middleware({
+        apiSpec: this.apiSpecLocation,
+        validateResponses: true,
+        ignorePaths: /\/api-docs/,
+      }),
+    );
+  }
 
-  return app;
+  private useRoutes(controllers: Array<IController>, pathRoute: string = '/') {
+    controllers.forEach((controller) =>
+      this.app.use(pathRoute, controller.getRoutes()),
+    );
+  }
+
+  private useCustomizers() {
+    this.app.use(
+      (err: any, req: Request, res: Response, next: NextFunction) => {
+        if (err instanceof OpenApiHttpError) {
+          return res.status(err.status).json({ message: err.message });
+        }
+    
+        return res.status(500).json({
+          message: 'An unexpected error occurred!'
+        });
+      }
+    );
+
+    if (!this.apiSpecLocation) {
+      return;
+    }
+
+    const swaggerDoc = YAML.parse(readFileSync(this.apiSpecLocation, 'utf8'));
+    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+  }
+  
+  async connectToDatabase() {
+    if (!this.database || !this.database.dbURI) {
+      console.error('Database not provided');
+      return;
+    }
+
+    mongoose.connect(this.database.dbURI, { dbName: this.database.dbName });
+  }
+
+  async closeDatabase() {
+    mongoose.disconnect();
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Delivery System listening on port ${this.port}`);
+    });
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,35 @@
-import mongoose from 'mongoose';
-import { createApp } from './app';
+import { App } from './app';
 import './configurations/dotenv';
 import * as env from './configurations/env-constants';
+import { RestaurantControllerFactory } from './configurations/factory/restaurant/restaurant.controller.factory';
+import { CategoryControllerFactory } from './configurations/factory/category/category.controller.factory';
+import { ProductControllerFactory } from './configurations/factory/product/product.controller.factory';
+import { OrderControllerFactory } from './configurations/factory/order/order.controller.factory';
+import path from 'path';
 
-const app = createApp();
+const OPEN_API_SPEC_FILE_PATH = path.resolve(
+  __dirname,
+  './contracts/contract.yaml',
+);
 
-mongoose.connect(env.DATABASE_URI, { dbName: env.DATABASE_NAME });
-
-app.listen(env.PORT, () => {
-  console.log(`Delivery System listening on port ${env.PORT}`);
+const app = new App({
+  port: env.PORT,
+  apiSpecLocation: OPEN_API_SPEC_FILE_PATH,
+  controllers: [
+    RestaurantControllerFactory.create(),
+    CategoryControllerFactory.create(),
+    ProductControllerFactory.create(),
+    OrderControllerFactory.create(),
+  ],
+  database: {
+    dbURI: env.DATABASE_URI,
+    dbName: env.DATABASE_NAME,
+  },
 });
+
+async function start() {
+  app.connectToDatabase();
+  app.listen();
+}
+
+start();
